80ae5c81bc657d2d204918753e73c63071abbb74
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-02-01 13:43:27 +0100

    Initial Commit

---
+++ Foodish/FoodishApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodishApp.swift
+//  Foodish
+//
+//  Created by Tony on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
